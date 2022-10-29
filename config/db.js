81ee8f3ae03bac1015bdb48daf54b754dfcf48b2
root <root@vultr.guest>
2022-10-29 07:28:53 +0000

    make some changes for list view and add

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 /*
- * @Author: wyf 844650674@qq.com
+ * @Author: Chuqi Fan
+ * @ID: 301090064
  * @Date: 2022-10-25 16:57:43
- * @LastEditors: wyf 844650674@qq.com
- * @LastEditTime: 2022-10-26 08:33:40
- * @FilePath: \COMP229.F2022.Midterm.Template-master\config\db.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @LastEditors: Chuqi Fan
+ * @LastEditTime: 2022-10-29 08:33:40
+ * @FilePath: \COMP229.F2022.Midterm\config\db.js
+ * @Description: database
  */
 // Do not expose your credentials in your code.
 let atlasDB = "mongodb+srv://fanchudi:fcd123@cluster0.q9mxvqd.mongodb.net/?retryWrites=true&w=majority";
